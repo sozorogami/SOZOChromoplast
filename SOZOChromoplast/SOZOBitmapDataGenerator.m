7f1cb7cb2d03e81f828e33ebcce30a77d1da016f
sozorogami <tyler.tape@gmail.com>
2015-10-11 13:34:04 -0400

    Fix crashes on 6 plus and memory leak issue
    A crash was occuring on the iPhone 6+ due to there being more bytes per pixel
    than other models (6 bytes vs. 4). Instead of trying to figure out
    which model we're running on, we leave it to Core Graphics to allocate a
    context of the correct size.
    
    Core Graphics also manages the memory that it allcoates for this
    context, meaning it is freed when the context ref is released. This
    solves the memory leak issue in #4.

--- SOZOChromoplast/SOZOBitmapDataGenerator.m
+++ SOZOChromoplast/SOZOBitmapDataGenerator.m
@@ -4,8 +4,11 @@
 
 + (NSArray *)bitmapDataForImage:(UIImage *)image {
     NSMutableArray *array = [NSMutableArray new];
-    unsigned char* cArray = [self rawBitmapDataForImage:image];
-    NSInteger numberOfPixels = (NSInteger) [image size].width * [image size].height;
+    CGSize imageSize = image.size;
+    CGContextRef context = [self ARGBContextWithSize:imageSize];
+    unsigned char* cArray = [self rawBitmapDataForImage:image inContext:context];
+
+    NSInteger numberOfPixels = (NSInteger) imageSize.width * imageSize.height;
     UIColor *color;
     for (int i = 0; i < numberOfPixels * 4; i += 4) {
         color = [UIColor colorWithRed:cArray[i+1]/255.f
@@ -14,19 +17,15 @@
                                 alpha:cArray[i]/255.f];
         [array addObject:color];
     }
+
+    CGContextRelease(context);
     return array;
 }
 
-+ (unsigned char *)rawBitmapDataForImage:(UIImage *)image {
-    CGContextRef contextRef = [self ARGBContextWithSize:image.size];
-    if (!contextRef) {
-        return NULL;
-    }
-
++ (unsigned char *)rawBitmapDataForImage:(UIImage *)image inContext:(CGContextRef)context {
     CGRect rect = CGRectMake(0, 0, image.size.width, image.size.height);
-    CGContextDrawImage(contextRef, rect, image.CGImage);
-    unsigned char *data = CGBitmapContextGetData(contextRef);
-    CGContextRelease(contextRef);
+    CGContextDrawImage(context, rect, image.CGImage);
+    unsigned char *data = CGBitmapContextGetData(context);
 
     return data;
 }
@@ -37,21 +36,12 @@
         [NSException raise:NSInternalInconsistencyException format:@"Error creating color space."];
     }
 
-    NSInteger bytesPerPixel = 4;
-    unsigned long bytesPerRow = size.width * bytesPerPixel;
-    unsigned long totalBytes = bytesPerRow * size.height;
-
-    void *bitmapData = malloc(totalBytes);
-    if (!bitmapData) {
-        [NSException raise:NSInternalInconsistencyException format:@"Memory allocation error."];
-    }
-
     // Cast to silence a type mismatch warning.
     // From the docs: The constants for specifying the alpha channel information are declared
     // with the `CGImageAlphaInfo` type but can be passed to [the bitmapInfo] parameter safely.
     CGBitmapInfo alphaSettings = (CGBitmapInfo) kCGImageAlphaPremultipliedFirst;
-    CGContextRef contextRef = CGBitmapContextCreate(bitmapData, size.width, size.height, 8,
-                                                    bytesPerRow, colorSpace, alphaSettings);
+    CGContextRef contextRef = CGBitmapContextCreate(NULL, size.width, size.height, 8,
+                                                    0, colorSpace, alphaSettings);
     if (!contextRef) {
         [NSException raise:NSInternalInconsistencyException format:@"Error creating context."];
     }
